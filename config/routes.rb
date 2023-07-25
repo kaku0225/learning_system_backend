0ed9c3b1cc187bee377f3c3ff5e24056843cd813
Eason <acontentedmind@gmail.com>
2023-07-25 15:37:16 +0800

    Remove: RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  namespace 'api' do
-    namespace 'v1' do
-      resources :users, only: [] do
-        collection do
-          post 'sign_up'
-          post 'login'
-          post 'send_reset_password_email'
-          post 'reset_password'
-        end
-      end
-    end
-  end
 end
