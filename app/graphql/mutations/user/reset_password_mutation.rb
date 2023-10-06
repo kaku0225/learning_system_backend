eb73c93a83cc07f44bbd7699050dba91b08c2ea7
Eason <acontentedmind@gmail.com>
2023-10-06 13:50:51 +0800

    Modify: refactor ResetPasswordMutation

--- app/graphql/mutations/user/reset_password_mutation.rb
+++ app/graphql/mutations/user/reset_password_mutation.rb
@@ -10,39 +10,30 @@ module Mutations
       field :message, String
 
       def resolve(token:, password:, password_confirmation:)
-        errors = check_token_and_find_user(token)
-        if @user.present?
-          reset_password(password, password_confirmation)
+        user = ::User.find_by_token_for(:password_reset, token)
+
+        if user.present?
+          reset_password(user, password, password_confirmation)
         else
-          { success: false, message: errors }
+          { success: false, message: I18n.t('messages.please_resend_the_password_reset_email') }
         end
       end
 
       private
 
-      def check_token_and_find_user(token)
-        JWT.decode token, Settings.jwt_hmac_secret, true, { algorithm: 'HS256' }
-        @user = ::User.find_by(reset_jti: token)
-        I18n.t('messages.invalid_token') if @user.blank?
-      rescue JWT::ExpiredSignature
-        I18n.t('messages.please_resend_the_password_reset_email')
-      rescue JWT::DecodeError
-        I18n.t('messages.invalid_token')
-      end
-
-      def reset_password(password, password_confirmation)
-        @user.assign_attributes(
+      def reset_password(user, password, password_confirmation)
+        user.assign_attributes(
           password: password,
           password_confirmation: password_confirmation,
           reset_jti: nil
         )
 
-        @user.create_or_update_password = true
+        user.create_or_update_password = true
 
-        if @user.save
-          { success: true, user: @user }
+        if user.save
+          { success: true, user: user }
         else
-          { success: false, message: @user.errors.full_messages.join('、') }
+          { success: false, message: user.errors.full_messages.join('、') }
         end
       end
     end
